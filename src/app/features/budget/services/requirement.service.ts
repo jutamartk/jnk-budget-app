6236a35808db9dbf71924f675e380ef232b6f61a
nune <jutamart.kot@gmail.com>
2023-12-07 22:44:43 +0700

    clean space commit

--- src/app/features/budget/services/requirement.service.ts
+++ src/app/features/budget/services/requirement.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-
 import { Observable } from 'rxjs';
 import { Requirement } from '../../models/requirement';
 
